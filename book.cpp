dd669a7ad883ac3aaf1d2acbe668bef9e4270270
Ashank-Bansi <bansiashank2006@gmail.com>
2025-10-26 19:35:06 -0400

    [ADD] Fichier book.cpp - debut du code avec les constructeurs, getters et setters

---
+++ book.cpp
@@ -0,0 +1,28 @@
+#include "book.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+// Default constructor
+Book::Book()
+    : title(""), author(""), isbn(""), isAvailable(true), borrowerName("") {}
+
+// Parameterized constructor
+Book::Book(const string& title, const string& author, const string& isbn)
+    : title(title), author(author), isbn(isbn), isAvailable(true), borrowerName("") {}
+
+// Getters
+string Book::getTitle() const { return title; }
+string Book::getAuthor() const { return author; }
+string Book::getISBN() const { return isbn; }
+bool Book::getAvailability() const { return isAvailable; }
+string Book::getBorrowerName() const { return borrowerName; }
+
+
+// Setters
+void Book::setTitle(const string& title) { this->title = title; }
+void Book::setAuthor(const string& author) { this->author = author; }
+void Book::setISBN(const string& isbn) { this->isbn = isbn; }
+void Book::setAvailability(bool isAvailable) { this->isAvailable = isAvailable; }
+void Book::setBorrowerName(const string& borrowerName) { this->borrowerName = borrowerName; }
