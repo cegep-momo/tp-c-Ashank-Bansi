bcbde8dfe5e006f02a088c953699c01adcee9009
Ashank-Bansi <bansiashank2006@gmail.com>
2025-10-26 20:20:23 -0400

    [ADD] ajout des methodes dans book.cpp

--- book.cpp
+++ book.cpp
@@ -26,3 +26,48 @@ void Book::setAuthor(const string& author) { this->author = author; }
 void Book::setISBN(const string& isbn) { this->isbn = isbn; }
 void Book::setAvailability(bool isAvailable) { this->isAvailable = isAvailable; }
 void Book::setBorrowerName(const string& borrowerName) { this->borrowerName = borrowerName; }
+
+//Methods
+void Book::checkOut(const string& borrower){
+    if (isAvailable) {
+        isAvailable = false;
+        borrowerName = borrower;
+    }
+}
+
+void Book::returnBook(){
+    isAvailable = true;
+    borrowerName.clear();
+}
+
+
+string Book::toString() const {
+    string result = "Titre: " + title + 
+                    "\nAuteur: " + author + 
+                    "\nISBN: " + isbn + 
+                    "\nDisponible: " + string(isAvailable ? "oui": "non");
+    
+    if (!isAvailable && !borrowerName.empty()) {
+        result += "\nEmprunteur: " + borrowerName;
+    }
+
+    return result; 
+}
+
+string Book::toFileFormat() const {
+    string result = title + "|" + author + "|" + isbn + "|" + (isAvailable ? "1" : "0") + ";" + borrowerName;
+}
+
+void Book::fromFileFormat(const string& line) {
+    stringstream ss(line);
+    string token;
+
+    getline(ss, title, '|');
+    getline(ss, author, '|');
+    getline(ss, isbn, '|');
+
+    getline(ss, token, '|');
+    isAvailable = (token == "1");
+
+    getline(ss, borrowerName, '|');
+}
